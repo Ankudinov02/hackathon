1200023d3a266ae12b46e7da8ab88d1f7cdf95bf
brakkk9 <bohdan.rak@auk.edu.ua>
2025-11-07 15:58:26 +0200

    Changed url to Render hosting

--- src/bot.py
+++ src/bot.py
@@ -8,18 +8,19 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def command_start_handler(message: Message) -> None:
-    await message.answer("Click the button to open the PineCV application.")
+    web_app_info = WebAppInfo(url="https://pinecv.onrender.com/")
+    button_text = "Launch"
+    web_app_button = KeyboardButton(text=button_text, web_app=web_app_info)
+
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[[web_app_button]], resize_keyboard=True
+    )
+    await message.answer(
+        "Click the button to open the PineCV application.",
+        reply_markup=keyboard,
+    )
 
 
 async def run(token: str) -> None:
     bot = Bot(token=token)
-
-    web_app_info = WebAppInfo(
-        url="https://pineapplebot-8f21292e4ac9.herokuapp.com/"
-    )
-    button_text = "Launch"
-    web_app_button = KeyboardButton(text=button_text, web_app=web_app_info)
-
-    _ = ReplyKeyboardMarkup(keyboard=[[web_app_button]], resize_keyboard=True)
-
     await dp.start_polling(bot)
